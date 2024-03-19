80e0a7433caa54df6eca9daf0a0eecac441bad11
zobia ansari <ansarimarsiya10@gmail.com>
2024-03-19 12:40:04 +0530

    another directory

--- shop/views.py
+++ shop/views.py
@@ -17,22 +17,33 @@ def index(request):
         allProds.append([prod, range(1, nSlides), nSlides])
 
     params={'allProds':allProds }
-    return render(request,"shop/index.html", params)
+    return render(request,"shop/index1.html", params)
 
 def about(request):
     return render(request,"shop/about.html")
 
 def contact(request):
-    return HttpResponse("we are in contact")
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("we are in tracker")
+    return render(request,"shop/tracker.html")
 
 def search(request):
-    return HttpResponse("we are in search")
+    return render(request,"shop/search.html")
+def productView(request, myid):
+    #Fetch the product using the id
+    product = Product.objects.filter(id=myid)
+    print(product)
 
-def productview(request):
-    return HttpResponse("we are in prodview")
+
+    return render(request, "shop/prodView.html", {'product':product[0]})
 
 def checkout(request):
-    return HttpResponse("we are in checkout")
+    return render(request,"shop/chckout.html")
